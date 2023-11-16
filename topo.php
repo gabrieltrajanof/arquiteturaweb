84fae70d4f1246b7ed41f18e0402970d04d61f08
Gabriel Trajano Duarte <gtrajano585@gmail.com>
2023-11-15 23:50:54 -0300

    Create topo.php
    Criando estrutura de topo e menu fixo.

---
+++ topo.php
@@ -0,0 +1,14 @@
+<header>
+<h1>Diego Porto</h1>
+</header>
+
+<div class="menu-lateral">
+<ul>
+  <li><a href="#sobre">Sobre</a></li>
+  <li><a href="#projetos">Projetos</a></li>
+  <li><a href="#servicos">Serviços</a></li>
+  <li><a href="#contato">Contato</a></li>
+  <li><a href="#faq">FAQ</a></li>
+</ul>
+</div>
+
