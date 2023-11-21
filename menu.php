e796a19af62dd8b43ad6e974a9f3e4de561d3c0b
Gabriel Trajano Duarte <gtrajano585@gmail.com>
2023-11-21 00:02:57 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <div class="menu">
 
 <a href="?pg=home">Home</a>
-<a href="?pg=precos">Preços</a>
+<a href="?pg=precos">Preço</a>
 <a href="?pg=novidades">Novidades</a>
 <a href="?pg=faleconosco">Contato</a>
 
